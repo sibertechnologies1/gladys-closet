f82408b8e400e855a1fdd69b6ec0dbc6e98e4128
sibertechnologies1 <sibertechnologies1@gmail.com>
2026-09-09 15:24:24 +0000

    I have updated the signup and login pages on where they each of them should redirect you to after successful signup and login

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -39,7 +39,7 @@ export default function Signup() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/shop`,
+        redirectTo: `${window.location.origin}/login`,
       },
     });
 
--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -52,7 +52,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/shop`,
+        redirectTo: `${window.location.origin}/`,
       },
     });
 
