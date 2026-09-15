46748f784a96b00a6d0c45947a5848c4d78a0bee
sibertechnologies1 <sibertechnologies1@gmail.com>
2026-09-15 17:23:36 +0000

    I have updated the navbar and footer imports on the on the OrderSuccess component

--- src/pages/OrderSuccess/OrderSuccess.jsx
+++ src/pages/OrderSuccess/OrderSuccess.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { FiCheckCircle, FiUserPlus, FiArrowRight } from 'react-icons/fi';
 import { supabase } from '../../lib/supabase';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from "../../components/Footer/Footer";
 
 export default function OrderSuccess() {
   const location = useLocation();
